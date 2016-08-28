5bac09c9b11996ca4aedfd1a3eabb34e74614a8e
Jason Campos <jcampos8782@gmail.com>
2016-08-28 00:06:16 -0700

    Use same jackson binary version as ninja version

--- src/main/java/me/jcampos/ninja/ext/modules/JsonModule.java
+++ src/main/java/me/jcampos/ninja/ext/modules/JsonModule.java
@@ -1,8 +1,11 @@
 package me.jcampos.ninja.ext.modules;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
@@ -22,9 +25,11 @@ import ninja.template.TemplateEngineJson;
  * @author Jason Campos <jcmapos8782@gmail.com>
  */
 public class JsonModule extends AbstractModule {
+	private static final Logger logger = LoggerFactory.getLogger(JsonModule.class);
 
 	@Override
 	protected void configure() {
+		logger.info("Intializing JsonModule");
 		bind(TemplateEngineJson.class);
 		bind(BodyParserEngineJson.class);
 
@@ -34,7 +39,7 @@ public class JsonModule extends AbstractModule {
 					@Override
 					public ObjectMapper get() {
 						final ObjectMapper objectMapper = new ObjectMapper();
-						objectMapper.registerModule(new JavaTimeModule());
+						objectMapper.registerModule(new JSR310Module());
 						objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 						return objectMapper;
 					}
